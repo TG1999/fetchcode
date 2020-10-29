b95dd97f75c152cc2e9f88d61f16051bc765f3a7
TG1999 <tushar.goel.dav@gmail.com>
2020-10-30 00:26:49 +0530

    Add setup files Add Author Add Changelog
    Signed-off-by: TG1999 <tushar.goel.dav@gmail.com>

--- setup.py
+++ setup.py
@@ -1,15 +1,6 @@
-from setuptools import setup, find_packages
+#!/usr/bin/env python
 
-with open('README.rst') as readme_file:
-    README = readme_file.read()
+import setuptools
 
-
-setup(name='fetchcode',
-    version='0.0.1',
-    description='Smart and reliable library to download code from HTTP, FTP and VCS URL',
-    long_description=README,
-    license='Apache-2.0',
-    packages=find_packages(),
-    author='nexb and others',
-    url='https://github.com/nexb/fetchcode'
-)
+if __name__ == "__main__":
+    setuptools.setup()
